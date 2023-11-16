0a175e8485c9b16df110b3b5a22454396e823094
kamepalli chandana <kamepallichandana1804@gmail.com>
2023-11-16 21:00:27 +0000

    added modules to repo

---
+++ modules/aks/aks_vars.tf
@@ -0,0 +1,9 @@
+variable "location" {
+    description =  "(Required) Keyvault Location"
+    default = "australiaeast"  
+}
+
+variable "rg_name" {
+    description =  "Name of the Resource Group"
+    default = "techslate-ade-rg"  
+}
